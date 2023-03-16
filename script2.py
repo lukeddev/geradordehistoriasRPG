6f28d28b53a6ade341bd8823a7cc8505d4893778
Lucas Daniel <102669848+lukeddev@users.noreply.github.com>
2023-03-16 15:13:12 -0300

    commit inicial

---
+++ script2.py
@@ -0,0 +1,58 @@
+import openai
+import random
+
+openai.api_key = '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'
+
+quantos = int(input('Quantos herois participarão dessa aventura? digite um número inteiro de 1 a 10: '))
+
+locais = [
+    'floresta',
+    'acampamento de bandidos',
+    'labirinto subterrâneo',
+    'cidade flutuante nas nuves',
+    'navio fantasma amaldiçoado',
+    'fortaleza de gelo nas montanhas',
+    'pântano assombrado',
+    'Iiha com um vulcão acordado',
+    'cidade abandonada no deserto',
+    'masmorra mágica, onde as paredes se movem, o tempo corre de maneira diferente e os aventureiros devem resolver quebra-cabeças para encontrar o caminho certo.'
+]
+onde = random.choice(locais)
+
+missão = [
+    'Investigar um mistério sombrio',
+    'Proteger um comerciante',
+    'Proteger um nobre',
+    'Encontrar um objeto mágico poderoso a pedido de um bruxo',
+    'Resgatar um refém importante',
+    'Defender um caravana de comerciantes',
+    'Descobrir um segredo antigo escondido',
+    'Escoltar um emissário ou líder político importante para uma conferência de paz',
+    'Impedir um ritual mágico malévolo',
+    'Ajudar uma figura importante a restaurar a justiça ou a vingar uma injustiça',
+]
+fazer = random.choice(missão)
+chefão = [
+    'dragão antigo e poderoso',
+    'Um lich malévolo',
+    'demônio das profundezas',
+    'titã colérico que foi despertado de um sono milenar',
+    'necromante obscuro',
+    'deus antigo e esquecido',
+    'rei tirânico, com um exercito poderoso',
+    'Um gênio maligno',
+    'Um arquimago insano',
+    'Um golem gigante',
+]
+boss = random.choice(chefão)
+aventura = ('Crie uma aventura de rpg onde {} aventureiros, devem ir até {}, e fazer {} O '
+            'chefão final será um {}' .format(quantos, onde, fazer, boss))
+
+completion = openai.Completion.create(
+    engine = "text-davinci-003",
+    prompt = aventura,
+    max_tokens=2018,
+    n=1,
+    temperature=1.0)
+reponse = completion.choices[0].text
+print(reponse)
